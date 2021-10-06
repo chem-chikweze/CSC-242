a90cc74674fe0fe62495fa6b651a935a6111f96e
chem <chem.chikweze@gmail.com>
2021-10-05 23:04:32 -0400

    Cleaned so I could star project 2

---
+++ Project 1/GameTree.java
@@ -0,0 +1,37 @@
+
+// import java.util.ArrayList;
+
+// public class GameTree {
+//     // A non generic implementation of the tree.
+
+//     State root;
+//     ArrayList<State> children;
+
+//     public GameTree(){
+
+//     }
+    
+
+//     public GameTree(State initialState) {
+//         this.root = initialState;
+//         this.children = new ArrayList<State>();
+//     }
+
+//     public State getRoot() {
+//         return root;
+//     }
+// }
+
+// // public class GameTree<T> {
+// //     private Node<T> root;   
+
+// //     public GameTree(T treeRoot) {
+// //         this.root = new Node<T>();
+// //         this.root.data = treeRoot;
+// //         this.root.children = new ArrayList<Node<T>>();
+// //     }   
+
+// //     public Node<T> getRoot() {
+// //         return root;
+// //     }
+// // }
